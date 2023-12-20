19259f560bf0a8712db6189c2b2436adf2ff82ba
“Jonathan <jonathan.valere@gmail.com>
2023-12-19 21:35:48 -0300

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <h1>React counter</h1>;
+  return (
+    <>
+      <Header />
+      <main></main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <i class="fa-solid fa-calculator"></i> React counter app
+    </header>
+  );
+};
+
+export default Header;
