063015fb8924519d09ebc1a4035eb1ec5f705a33
“Jonathan <jonathan.valere@gmail.com>
2023-12-20 10:37:22 -0300

    Code improvement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,24 @@
 const Footer = () => {
   return (
     <footer>
-      Made with <span>React</span> at <span>Le Reacteur</span> by{" "}
-      <span>Jonathan VALERE</span>
+      Made with{" "}
+      <span>
+        <a href="https://react.dev/" target="_blank">
+          React
+        </a>
+      </span>{" "}
+      at{" "}
+      <span>
+        <a href="https://www.lereacteur.io/" target="_blank">
+          Le Reacteur
+        </a>
+      </span>{" "}
+      by{" "}
+      <span>
+        <a href="https://github.com/jonathanValere" target="_blank">
+          Jonathan VALERE
+        </a>
+      </span>
     </footer>
   );
 };
